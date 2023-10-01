95eb6d253ec1207e2d709edec2bd9621b6bc5922
Rajaa C <96713906+RC2110@users.noreply.github.com>
2023-10-02 00:34:21 +0530

    developed a fair weatherforecast dashboard appl

--- functions.py
+++ functions.py
@@ -5,8 +5,7 @@ def get_data(place, days=None, kind=None):
     url=f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     content = response.json()
-    temperatures = [i['main']['temp'] for i in content['list']]
-    return content, temperatures
+    return content
 
 if __name__ == "__main__":
     print(get_data("Madurai"))
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from functions import get_data
 
 # st.set_page_config(layout="wide")
 st.title("An interactive weather forecast app")
@@ -7,15 +8,30 @@ location = st.text_input(label="Place:", placeholder="Enter a place", help="Ente
 days = st.slider(label="Select days", min_value=1, max_value=5, step=1)
 options= st.selectbox(label='Choose temperature or day', placeholder="Enter a key", options=["Temperature", "Sky"])
 
+try:
+    if location:
+        st.write(f"{location}'s {options} for the next {days} days is ")
 
-st.write(f"{inpt}'s {options} for the next {days} days is ")
 
-temperatures = get_graph(location, days, options)
-newtemp= [i * days for i in temperatures]
-figure = px.line(x=date, y= newtemp)
-st.plotly_chart(figure)
+        daysin3hr = int(days) * 9
+        content = get_data(location, days, options)
+        days= [i['dt_txt']for i in content['list']][0:daysin3hr]
 
-get_graph(days)
+        if options == "Temperature":
+            temperatures = [i['main']['temp'] for i in content['list']][0:daysin3hr]
+            temperatures= [i / 10 for i in temperatures]
+            figure = px.line(x=days, y=temperatures)
+            st.plotly_chart(figure)
+        elif options == "Sky":
+            temperatures = [i['weather'][0]['main']for i in content['list']][0:daysin3hr]
+            img_url=[f"Images/{i.lower()}.png" for i in temperatures]
+            st.image(img_url,width=150)
+
+
+except KeyError:
+    st.info("Make sure the location is spelled right!")
+
+
 
 
 
